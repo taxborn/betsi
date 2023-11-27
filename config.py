946a04155383bc4136fad5210bd83fd5861d16ae
Braxton Fair <hello@taxborn.com>
2023-11-27 08:14:29 -0600

    Add training output

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size": 16,
+        "batch_size": 8,
         "num_epochs": 20,
         "lr": 10**-4,
         "seq_len": 350,
-        "d_model": 256,
+        "d_model": 512,
         "datasource": 'opus_books',
         "lang_src": "en",
         "lang_tgt": "it",
